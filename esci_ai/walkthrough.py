6dd460ebc1a8e4ec39c6221f2a787ddfbce3af2c
keinan <24482967+keinan1@users.noreply.github.com>
2026-02-13 16:27:10 -0500

    complete walkthrough notebook

--- esci_ai/walkthrough.py
+++ esci_ai/walkthrough.py
@@ -8,110 +8,16 @@ app = marimo.App(width="medium")
 def _():
     import marimo as mo
 
-    return (mo,)
-
-
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md(r"""
-    # ESCI-AI Walkthrough
-
-    ## Problem
-
-    The ESCI dataset consists of a set of query-product pairs.
-    - Items labelled "E" are deemed "relevant for the query and satisfies __all__ the query specifications."
-    - However, on inspection, some "E" items do not satisfy all the query specifications.
-
-    The task is to identify examples where "E" is misapplied for three specific queries (given below). Specifically:
-    - Identify when the query is incorrect for the product.
-    - Reformulate the query so that it accurately reflects the product.
-
-    ## Plan and Constraints
-
-    While the task is focused on a small subset of the original dataset (less than 20 observations), the solution will only be of practical use if it can be used on the entire set of ~2 million "E" queries to identify the mismatches.
-
-    For example, the three outputs -- the product, the close-but-incorrect query, and the corrected query -- can be used to finetune an embedding model to move exact matches closer in the embedding space.
-
-    The problem breaks into two different kinds of tasks:
-
-    - [Task 1]: Binary classification - identify when the query is incorrect
-    - [Task 2]: Text Generation - reformulate the query
-
-    The first task can in theory be performed efficiently on 2 million examples if we trained a BERT model on query-product pairs, predicting match/no-match. Since we don't have those labels, we'd use the more expensive LLM approach to generate enough balanced labels (~1000) to train the BERT model.
-
-    The second task requires text generation, so we'll assume LLMs will be required even in the scaled solution. However, the number of cases will be relatively small, since we only have to do this for the mismatched pairs.
-
-    The only goal for this task is to get a working solution for subset of 20 examples. However, we'll build with the points above in mind. In concrete terms:
-
-    - Aim to use a local LLM, as small as possible to get accurate results. We'll start with [Ministral-3-8b-Instruct](https://huggingface.co/mistralai/Ministral-3-8B-Instruct-2512) as a baseline with the option to move up to the 14B model, or down to the 3B model depending on results.
-    - Keep the implementation modular by separating out the classification and the text generation tasks. This will allow use of the first component just to scale up (generate labels to train a classifier) and the second to be reused in the scaled solution. It waill also allow using different LLM models if needed between the two tasks.
-
-    ## Development Approach
-
-    We'll use the following development process:
-
-    - Step 1: build quick prototype
-    - Step 2: perform error analysis on traces
-    - Step 3: create automated evals on results
-    - Step 4: iterate until all examples are corrrect
-
-    ## Tooling
-
-    - `uv` for package management
-    - `ollama` for local model inference
-    - `pydantic` for structured outputs
-    - `pydantic-ai` for lightweight llm orchestration
-    - `pydantic-logfire` for llm observability
-    - `pydantic-evals` to create and run automated evals
-
-    ## Setup
-
-    - Clone the repo
-
-    - If not already installed:
-        - Install `uv`
-
-        ```shell
-        curl -LsSf https://astral.sh/uv/install.sh | sh
-        ```
-
-        - Install `ollama` and download the model
-
-        ```shell
-        curl -fsSL https://ollama.com/install.sh | sh
-        ollama pull ministral-3:8b # ollama pull gpt-oss:latest
-        ```
-
-    - Create a `.env` file (optional: only needed for pydantic-logfire integration, ask me for a key)
-
-        ```shell
-        touch .env
-        echo "PYDANTIC_LOGFIRE=<...project-api-key...>"
-        ```
-
-    - Download and process the esci data:
-
-        ```shell
-        uv run esci_ai/setup/download_data.py
-        uv run esci_ai/setup/process_data.py
-        ```
-
-       The processed data will be saved as parquet files in `data/processed/`.
-
-
-    - A bit self-referential: to run or edit this Marimo notebook:
-
-        ```shell
-        uv run marimo run esci_ai/walkthrough.py # read only
-        uv run marimo edit esci_ai/walkthrough.py # edit
-        ```
-    """)
     return
 
 
 @app.cell
 def _():
+    # examine full esci dataset
+    # requires that esci data is downloaded and processed - see README setup instructions
+
     from pathlib import Path
+
     import polars as pl
 
     project_dir = Path(__file__).parent.parent
@@ -120,57 +26,63 @@ def _():
     df_all = pl.read_parquet(data_dir / "processed" / "examples_products.parquet")
 
     df_all.glimpse()
+    return data_dir, df_all, pl
 
+
+@app.cell
+def _(df_all, pl):
     # total examples by esci label
     df_all.group_by("esci_label").agg(
         count=pl.len(),
         unique_queries=pl.col("query_id").unique().count(),
         unique_products=pl.col("product_id").unique().count(),
     )
-    return data_dir, pl
+    return
 
 
 @app.cell
 def _(data_dir, pl):
-    df = pl.read_parquet(
+    # examine subset of three queries
+
+    df_subset = pl.read_parquet(
         data_dir / "processed" / "examples_products_subset.parquet"
     )
 
-    df
-    return (df,)
+    df_subset
+    return (df_subset,)
 
 
 @app.cell
 def _():
-    # by manual inspection alone, these are at least some of the incorrect observations (7/24 error rate)
+    # by manual inspection, these are the non-exact-matches (33% error rate)
 
-    incorrect_example_ids = [
+    negative_example_ids = [
         # batteries
         142660,  # 60 count, not 100 count
         142666,  # AAA, not AA
         # drills
         660823,  # no mention of gyroscopic
         660827,  # no mention of gyroscopic
-        660840,  # charger only, no drill
+        660838,  # charger only, no drill
         # paper
         1163629,  # matte, not glossy
+        1163634,  # ambiguous: brand field lists Doaaler, title lists Kodak
         1163641,  # matte, not glossy
     ]
-    return
+    return (negative_example_ids,)
 
 
 @app.cell
 def _():
     # define pydantic models to structure the task inputs (query-product examples) and outputs (query match, query reformulation)
 
-    from pydantic import BaseModel, Field
     from enum import Enum
 
+    from pydantic import BaseModel, Field
 
     class QueryInfo(BaseModel):
         query_id: int
         query: str
-
 
     class ProductInfo(BaseModel):
         product_id: str
@@ -180,11 +92,9 @@ def _():
         product_brand: str
         product_color: str | None = None
 
-
     class MatchClassification(Enum):
         EXACT_MATCH = "exact_match"
         NOT_EXACT_MATCH = "not_exact_match"
-
 
     class QueryProductMatch(BaseModel):
         match_classification: MatchClassification = Field(
@@ -193,16 +103,14 @@ def _():
         )
         reasoning: str = Field(
             ...,
-            description=f"""Succinct reason for the classification. If classified as a {MatchClassification.EXACT_MATCH}, return 'All query specifications satisfied by the product.' If classified as {MatchClassification.NOT_EXACT_MATCH}, list which query specifications are not satisfied by the product.""",
+            description=f"Succinct reason for the classification. If classified as a {MatchClassification.EXACT_MATCH}, return 'All query specifications satisfied by the product.' If classified as {MatchClassification.NOT_EXACT_MATCH}, cite precisely the query specification(s) not satisfied by the product.",
         )
-
 
     class CorrectQuery(BaseModel):
         correct_query: str = Field(
             ...,
             description="Given the product information, formulate a query for which the product would be an exact match",
         )
-
 
     class QueryProductExample(BaseModel):
         example_id: int
@@ -211,12 +119,18 @@ def _():
         query_product_match: QueryProductMatch | None = None
         query_correction: CorrectQuery | None = None
 
-    return ProductInfo, QueryInfo, QueryProductExample, QueryProductMatch
+    return (
+        MatchClassification,
+        ProductInfo,
+        QueryInfo,
+        QueryProductExample,
+        QueryProductMatch,
+    )
 
 
 @app.cell
-def _(ProductInfo, QueryInfo, QueryProductExample, df):
-    # create list of QueryProductExamples from df
+def _(ProductInfo, QueryInfo, QueryProductExample, df_subset):
+    # create list of QueryProductExamples from df_subset
 
     examples = [
         QueryProductExample(
@@ -234,7 +148,7 @@ def _(ProductInfo, QueryInfo, QueryProductExample, df):
                 product_color=row.get("product_color", ""),
             ),
         )
-        for row in df.to_dicts()
+        for row in df_subset.to_dicts()
     ]
 
     examples[:5]
@@ -244,13 +158,19 @@ def _(ProductInfo, QueryInfo, QueryProductExample, df):
 @app.cell
 async def _():
     # test agent setup
-    # requires .env with ollama base url: see setup instructions
+    # requires .env with ollama base url, and downloaded models - see README setup instructions
 
-    from dotenv import find_dotenv, load_dotenv
-    from pydantic_ai import Agent
     from pprint import pprint
 
+    import logfire
+    from dotenv import find_dotenv, load_dotenv
+    from pydantic_ai import Agent
+
     load_dotenv(find_dotenv())
+
+    # logfire for observability, not essential to run
+    logfire.configure()
+    logfire.instrument_pydantic_ai()
 
     test_settings = {
         "temperature": 0,
@@ -258,7 +178,7 @@ async def _():
     }
 
     test_agent = Agent(
-        model="ollama:gpt-oss:20b",
+        model="ollama:ministral-3:3b",
         model_settings=test_settings,
     )
 
@@ -269,19 +189,21 @@ async def _():
 
 @app.cell
 def _(Agent, QueryProductMatch):
+    # create classifier agent
+
     classifier_system_prompt = """
     <TASK OVERVIEW>
-    You are a quality assurance agent helping to improve a product query system. /
+    You are a world-class quality assurance agent responsible for improving a product query system. /
     Your task is to examine a query-product pair, and determine whether the product exactly matches the query.
     </TASK OVERVIEW>
 
     <INSTRUCTIONS>
     To accomplish this task:
 
-    1. Consider all the explicit specifications detailed in the query
-    2. Examine all the explicit product details
+    1. Consider all the explicit specifications detailed in the query.
+    2. Examine all the explicit product details.
     3. Determine whether all query specifications are explicitly met in the product details.
-    4. Output your final answer with appropriate explanation.
+    4. Output your final answer with a clear, fact-based explanation.
     </INSTRUCTIONS>
     """
 
@@ -291,7 +213,10 @@ def _(Agent, QueryProductMatch):
     }
 
     classifier_agent = Agent(
-        model="ollama:gpt-oss:20b",
+        model="ollama:ministral-3:3b",
+        # model="ollama:ministral-3:8b",
+        # model="ollama:ministral-3:14b",
+        # model="ollama:gpt-oss:20b",
         model_settings=model_settings,
         max_concurrency=10,
         retries=2,
@@ -303,7 +228,7 @@ def _(Agent, QueryProductMatch):
 
 @app.cell
 async def _(classifier_agent, examples, pprint):
-    # test example
+    # classifier test example
 
     _e = examples[4]
 
@@ -327,9 +252,10 @@ async def _(classifier_agent, examples, pprint):
 @app.cell
 async def _(classifier_agent, examples):
     # run all classification examples
-    # see https://ai.pydantic.dev/agent/#__tabbed_9_1
+    # for pydantic-ai async agent run, see https://ai.pydantic.dev/agent/#__tabbed_9_1
 
     import asyncio
+    import time
 
     # create prompts
     match_prompts = [
@@ -344,13 +270,68 @@ async def _(classifier_agent, examples):
         for e in examples
     ]
     # batch run classifier agent
+    start = time.time()
     match_results = await asyncio.gather(
         *[classifier_agent.run(mp) for mp in match_prompts]
     )
+    end = time.time()
+    total_time = end - start
+    print(f"Execution time for {len(examples)} items: {round(total_time)} seconds")
+    print(f"Average {round(total_time / len(examples))} seconds per item")
+    return (match_results,)
+
+
+@app.cell
+def _(
+    MatchClassification,
+    examples,
+    match_results,
+    negative_example_ids,
+    pprint,
+):
+    # measure performance
+
     # add classification to examples
-    # note: asyncio.gather guarantees list order, so this zip pairing is safe
     for e, mr in zip(examples, match_results):
         e.query_product_match = mr.output
+
+    # separate classifications
+    exact_matches = [
+        e
+        for e in examples
+        if e.query_product_match.match_classification.value
+        == MatchClassification.EXACT_MATCH.value
+    ]
+    not_exact_matches = [
+        e
+        for e in examples
+        if e.query_product_match.match_classification.value
+        == MatchClassification.NOT_EXACT_MATCH.value
+    ]
+    true_positives = [
+        e for e in exact_matches if e.example_id not in negative_example_ids
+    ]
+    false_positives = [e for e in exact_matches if e.example_id in negative_example_ids]
+    true_negatives = [
+        e for e in not_exact_matches if e.example_id in negative_example_ids
+    ]
+    false_negatives = [
+        e for e in not_exact_matches if e.example_id not in negative_example_ids
+    ]
+
+    accuracy = (len(true_positives) + len(true_negatives)) / (len(examples))
+    precision = len(true_positives) / (len(true_positives) + len(false_positives))
+    recall = len(true_positives) / (len(true_positives) + len(false_negatives))
+
+    pprint(f"ACCURACY: {accuracy}")
+    pprint(f"PRECISION: {precision}")
+    pprint(f"RECALL: {recall}")
+    pprint("FALSE POSITIVES:")
+    for f in false_positives:
+        pprint(f)
+    pprint("FALSE NEGATIVES:")
+    for f in false_negatives:
+        pprint(f)
     return
 
 
